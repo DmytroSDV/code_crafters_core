cc36224ee15242e839993630258a79f11567f188
Cr1VS <109921577+Cr1VS@users.noreply.github.com>
2023-12-16 22:52:12 +0200

    del

--- RecordData.py
+++ RecordData.py
@@ -1,7 +1,3 @@
 # Hi, Im team member DmKrvts, will do my best here
 
-print("I wanna to add Name class!")
-print("I wanna to add Name class!")
-print("I wanna to add Name class!")
-print("I wanna to add Name class!")
-print("I wanna to add Name class!")
+
