130f2f60162bf21236c1c60e72781e63c4a9dc33
IVadymMarchenko <topim31@gmail.com>
2023-12-17 21:52:12 +0300

    класс нейм

--- RecordData.py
+++ RecordData.py
@@ -1,3 +1,20 @@
-# Hi, Im team member DmKrvts, will do my best here
-print("hello")
-user_input = input("")
+
+class Name:
+
+    def __init__(self,name):
+        self.set_name(name)
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self,name):
+        if self.valid_name(name):
+            self.name=name
+        else:
+            raise ValueError('Wrong Name. Name needs to contain not less than 3 symbols')
+
+    def valid_name(self,name):
+        if len(name)>=3 and name.isalpha():
+            return True
+        return False
+
