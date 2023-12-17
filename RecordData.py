6a700515db4d648c59d25e9d72016209c89c4db3
Cr1VS <109921577+Cr1VS@users.noreply.github.com>
2023-12-17 16:29:34 +0200

    Class/email

--- RecordData.py
+++ RecordData.py
@@ -1,3 +1,11 @@
-# Hi, Im team member DmKrvts, will do my best here
-print("hello")
-user_input = input("")
+import re
+
+
+class Email:
+    pattern = r"[a-zA-Z]{1}[\w\.]+@[a-zA-Z]+\.[a-zA-Z]{2,}"
+
+    def __init__(self, email):
+        if re.match(self.pattern, email):
+            self.email = email
+        else:
+            raise ValueError("Incorrect email! Please provide correct email.")
