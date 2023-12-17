88e460ed2a6e1b54ec803e394b9bc83df8bee6bf
IVadymMarchenko <topim31@gmail.com>
2023-12-17 23:50:58 +0300

    adress book

--- AdressBook.py
+++ AdressBook.py
@@ -1,1 +1,45 @@
+from Record import Record
+from collections import UserList
+
+
+class AddressBook(UserList):
+
+    def __init__(self,addressbook):
+        self.addressbook=addressbook
+        self.id=1
+
+    def add_contacts(self):
+        name=input('Please enter name: ')
+        phone=input('Please enter phone: ')
+        record=Record(name)
+        record.add(phone)
+        birthday=input('Please enter birthday: ')
+        email=input('Please enter email: ')
+        record.add_mail(email)
+        note=input('Please enter note: ')
+        record.add_note(note)
+        if birthday:
+            year, month, day = map(int, birthday.split())
+            record.birthday = Birthday(year, month, day)
+
+        contacts={'id':self.id,'name': record.name,'phone':record.phones, 'birthday': record.birthday,'email': record.email,'note': record.note }
+        self.data.append(contacts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
