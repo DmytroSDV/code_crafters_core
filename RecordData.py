1e9c4fa39cc0d79080fb1a6a675201827a424e85
NataliaSklyarenko <143900453+NataliaSklyarenko@users.noreply.github.com>
2023-12-17 20:29:23 +0200

    Update RecordData.py
    Оновила RecordData.py

--- RecordData.py
+++ RecordData.py
@@ -1,3 +1,9 @@
-# Hi, Im team member DmKrvts, will do my best here
-print("hello")
-user_input = input("")
+class Phone(Field):
+    def __init__(self, value):
+        if not self.is_valid_phone(value):
+            raise ValueError("Invalid phone number format")
+        super().__init__(value)
+
+    def is_valid_phone(self, phone):
+    
+        return bool(re.match(r'^[\d-]+$', phone))
