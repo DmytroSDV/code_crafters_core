41ac3c078fb69c80efd38dc49e52726e305802eb
NataliaSklyarenko <143900453+NataliaSklyarenko@users.noreply.github.com>
2023-12-17 21:50:48 +0200

    Update Record.py
    Додала клас Record

---
+++ Record.py
@@ -0,0 +1,21 @@
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for p in self.phones:
+            if p.value == old_phone:
+                p.value = new_phone
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
